72ea66cc79402774dc2118c883bb26e86372ce56
Jamey Sharp <jamey@minilop.net>
2025-07-27 22:45:27 -0700

    Extract watcher loop to top-level function
    No functional changes; this commit is much easier to read if you ignore
    whitespace changes.

--- gawkurl-core/src/lib.rs
+++ gawkurl-core/src/lib.rs
@@ -67,169 +67,163 @@ impl Pages {
                 .write()
                 .unwrap()
                 .entry(uri.to_owned())
-                .or_insert_with_key(|uri| Self::watch(self.client.clone(), uri.clone()))
+                .or_insert_with_key(|uri| {
+                    let sender = Sender::new(Page::default());
+                    tokio::spawn(watch_url(self.client.clone(), uri.clone(), sender.clone()));
+                    sender
+                })
                 .subscribe()
         }
     }
+}
 
-    fn watch(client: Client, uri: String) -> Sender<Page> {
-        let sender = Sender::new(Page::default());
-        let result = sender.clone();
-        tokio::spawn(async move {
-            const DAY: u64 = 86400;
+async fn watch_url(client: Client, uri: String, sender: Sender<Page>) -> ! {
+    const DAY: u64 = 86400;
 
-            // validators provided by the server
-            let mut etag = None;
-            let mut last_modified: Option<HttpDate> = None;
+    // validators provided by the server
+    let mut etag = None;
+    let mut last_modified: Option<HttpDate> = None;
 
-            // last_fetched is used to decide if the last-modified header is
-            // plausible. if the page changed after our previous fetch, but
-            // last-modified says it changed before that, it's nonsense. before
-            // fetching the page the very first time, initialize this to a date
-            // that's after most time epochs, including the Unix epoch in 1970
-            // and the DOS epoch in 1980, so that if a file has a timestamp of
-            // "0" on such systems we know to ignore it. but this date should be
-            // before any web page somebody might want to monitor, so since HTTP
-            // was introduced around 1991, 1990 seems like a good choice.
-            // https://en.wikipedia.org/wiki/Epoch_(computing)#Notable_epoch_dates_in_computing
-            let mut last_fetched =
-                SystemTime::UNIX_EPOCH + Duration::from_secs(20 * (365 * 4 + 1) / 4 * DAY);
+    // last_fetched is used to decide if the last-modified header is
+    // plausible. if the page changed after our previous fetch, but
+    // last-modified says it changed before that, it's nonsense. before
+    // fetching the page the very first time, initialize this to a date
+    // that's after most time epochs, including the Unix epoch in 1970
+    // and the DOS epoch in 1980, so that if a file has a timestamp of
+    // "0" on such systems we know to ignore it. but this date should be
+    // before any web page somebody might want to monitor, so since HTTP
+    // was introduced around 1991, 1990 seems like a good choice.
+    // https://en.wikipedia.org/wiki/Epoch_(computing)#Notable_epoch_dates_in_computing
+    let mut last_fetched =
+        SystemTime::UNIX_EPOCH + Duration::from_secs(20 * (365 * 4 + 1) / 4 * DAY);
 
-            // our best guess of the last time this page changed, taking into
-            // account what time it was when we fetched the latest version, as
-            // well as the last-modified header, if we trust it.
-            let mut last_changed = Instant::now();
-            let mut last_hash = ContentHash::default();
+    // our best guess of the last time this page changed, taking into
+    // account what time it was when we fetched the latest version, as
+    // well as the last-modified header, if we trust it.
+    let mut last_changed = Instant::now();
+    let mut last_hash = ContentHash::default();
 
-            loop {
-                // fetch and hash the page
-                // TODO: ratelimit requests per domain
-                let mut request = client.get(&uri);
-                if let Some(etag) = &etag {
-                    request = request.header(header::IF_NONE_MATCH, etag);
-                }
-                if let Some(last_modified) = &last_modified {
-                    request = request.header(header::IF_MODIFIED_SINCE, last_modified.to_string());
-                }
+    loop {
+        // fetch and hash the page
+        // TODO: ratelimit requests per domain
+        let mut request = client.get(&uri);
+        if let Some(etag) = &etag {
+            request = request.header(header::IF_NONE_MATCH, etag);
+        }
+        if let Some(last_modified) = &last_modified {
+            request = request.header(header::IF_MODIFIED_SINCE, last_modified.to_string());
+        }
 
-                // if we don't get a server-provided max-age in response to
-                // this request, treat it like the response can be revalidated
-                // immediately.
-                let mut max_age = 0;
+        // if we don't get a server-provided max-age in response to
+        // this request, treat it like the response can be revalidated
+        // immediately.
+        let mut max_age = 0;
 
-                let request_time = SystemTime::now();
-                if let Ok(response) = request.send().await {
-                    let response_time = SystemTime::now();
+        let request_time = SystemTime::now();
+        if let Ok(response) = request.send().await {
+            let response_time = SystemTime::now();
 
-                    let headers = response.headers();
+            let headers = response.headers();
 
-                    // the date header indicates what time the origin server
-                    // generated this reponse, and if present allows us to
-                    // interpret other date headers correctly even if our clock
-                    // disagrees with theirs; but if it's absent we can guess
-                    // our clock is probably close enough.
-                    let date =
-                        parse::<HttpDate>(headers, header::DATE).map_or(response_time, Into::into);
+            // the date header indicates what time the origin server
+            // generated this reponse, and if present allows us to
+            // interpret other date headers correctly even if our clock
+            // disagrees with theirs; but if it's absent we can guess
+            // our clock is probably close enough.
+            let date = parse::<HttpDate>(headers, header::DATE).map_or(response_time, Into::into);
 
-                    let age = compute_age(headers, request_time, response_time);
+            let age = compute_age(headers, request_time, response_time);
 
-                    // the origin can specify an expiration time in
-                    // two different ways: either the HTTP/1.1 way with
-                    // `cache-control: max-age=N`, where N is the number of
-                    // seconds to consider the response still valid for, or
-                    // with the HTTP/1.0 `expires` header containing a date. per
-                    // https://datatracker.ietf.org/doc/html/rfc7234#section-5.3
-                    // we prefer max-age over expires.
-                    if let Some(v) = parse_max_age(headers).or_else(|| parse_expires(headers, date))
-                    {
-                        max_age = v.saturating_sub(age)
-                    }
+            // the origin can specify an expiration time in
+            // two different ways: either the HTTP/1.1 way with
+            // `cache-control: max-age=N`, where N is the number of
+            // seconds to consider the response still valid for, or
+            // with the HTTP/1.0 `expires` header containing a date. per
+            // https://datatracker.ietf.org/doc/html/rfc7234#section-5.3
+            // we prefer max-age over expires.
+            if let Some(v) = parse_max_age(headers).or_else(|| parse_expires(headers, date)) {
+                max_age = v.saturating_sub(age)
+            }
 
-                    // per https://datatracker.ietf.org/doc/html/rfc7234#section-4.3.3,
-                    // we're allowed to pretend like 5xx server errors were actually
-                    // the server just not responding, and reuse a cached response
-                    // instead. I'm choosing to do the same for all non-200 responses
-                    // but it's not clear yet if that's the right plan for this use.
-                    if response.status() == StatusCode::OK {
-                        etag = headers.get(header::ETAG).cloned();
-                        last_modified = parse(headers, header::LAST_MODIFIED);
+            // per https://datatracker.ietf.org/doc/html/rfc7234#section-4.3.3,
+            // we're allowed to pretend like 5xx server errors were actually
+            // the server just not responding, and reuse a cached response
+            // instead. I'm choosing to do the same for all non-200 responses
+            // but it's not clear yet if that's the right plan for this use.
+            if response.status() == StatusCode::OK {
+                etag = headers.get(header::ETAG).cloned();
+                last_modified = parse(headers, header::LAST_MODIFIED);
 
-                        let contents = response.bytes().await.unwrap();
+                let contents = response.bytes().await.unwrap();
 
-                        let hash_bytes = ContentHasher::digest(&contents);
-                        let mut hash = ContentHash::default();
-                        let hash_len = base64::engine::general_purpose::URL_SAFE_NO_PAD
-                            .encode_slice(&hash_bytes, &mut hash);
-                        debug_assert_eq!(hash_len, Ok(hash.len()));
+                let hash_bytes = ContentHasher::digest(&contents);
+                let mut hash = ContentHash::default();
+                let hash_len = base64::engine::general_purpose::URL_SAFE_NO_PAD
+                    .encode_slice(&hash_bytes, &mut hash);
+                debug_assert_eq!(hash_len, Ok(hash.len()));
 
-                        if last_hash != hash {
-                            last_hash = hash;
-                            sender.send_replace(Page { hash, contents });
-                            last_changed = Instant::now();
+                if last_hash != hash {
+                    last_hash = hash;
+                    sender.send_replace(Page { hash, contents });
+                    last_changed = Instant::now();
 
-                            // TODO: parse XML and look for RSS or Atom timestamps
+                    // TODO: parse XML and look for RSS or Atom timestamps
 
-                            // last-modified should only be compared to
-                            // times measured using the server's clock, but
-                            // last_fetched uses our local clock, so turn both
-                            // into "seconds before now" in the appropriate
-                            // clock domain before comparing them.
-                            if let Some(last_modified) = last_modified {
-                                if let Ok(last_modified_age) =
-                                    date.duration_since(last_modified.into())
-                                {
-                                    let last_modified_age =
-                                        last_modified_age.saturating_add(Duration::from_secs(age));
-                                    if let Ok(last_fetched_age) =
-                                        request_time.duration_since(last_fetched)
-                                    {
-                                        if last_modified_age < last_fetched_age {
-                                            last_changed -= last_modified_age;
-                                        }
-                                    }
+                    // last-modified should only be compared to
+                    // times measured using the server's clock, but
+                    // last_fetched uses our local clock, so turn both
+                    // into "seconds before now" in the appropriate
+                    // clock domain before comparing them.
+                    if let Some(last_modified) = last_modified {
+                        if let Ok(last_modified_age) = date.duration_since(last_modified.into()) {
+                            let last_modified_age =
+                                last_modified_age.saturating_add(Duration::from_secs(age));
+                            if let Ok(last_fetched_age) = request_time.duration_since(last_fetched)
+                            {
+                                if last_modified_age < last_fetched_age {
+                                    last_changed -= last_modified_age;
                                 }
                             }
                         }
-                    } else {
-                        // handling 304 Not Modified responses is specified in
-                        // https://datatracker.ietf.org/doc/html/rfc7234#section-4.3.4
-                        // but because we only store one response at a time and
-                        // never add synthetic if-modified-since headers, the
-                        // cases boil down to this: if the response has an etag and
-                        // it matches the etag we have from a previous response,
-                        // then we should update our stored copy's headers from
-                        // the new response. same goes for last-modified. but
-                        // if those headers match then there's nothing to update
-                        // since we don't track any other headers yet. so, like
-                        // 5xx responses, 304 should preserve the old response
-                        // unchanged.
                     }
-
-                    // only count it as a successful fetch if we actually got
-                    // a response from the server, because when we have network
-                    // trouble or similar, it's possible that we miss a change.
-                    last_fetched = request_time;
                 }
-
-                // https://datatracker.ietf.org/doc/html/rfc7234#section-4.2.2
-                let last_changed_age = Instant::now().duration_since(last_changed).as_secs();
-                let heuristic_freshness = last_changed_age / 10;
-
-                // now we have two hints about the minimum time we should wait
-                // before checking again. max-age is the origin server's hint
-                // that a cache should not revalidate before that many seconds
-                // have elapsed. and our heuristic freshness is an estimate
-                // of how long we think it might be before it's worth checking
-                // again. so wait for whichever hint is longer, but also clamp
-                // to a reasonable range that neither waits forever nor sends
-                // another request immediately.
-                let wait = heuristic_freshness.max(max_age).clamp(600, 7 * DAY);
-
-                // sleep, then loop
-                tokio::time::sleep(Duration::from_secs(wait)).await;
+            } else {
+                // handling 304 Not Modified responses is specified in
+                // https://datatracker.ietf.org/doc/html/rfc7234#section-4.3.4
+                // but because we only store one response at a time and
+                // never add synthetic if-modified-since headers, the
+                // cases boil down to this: if the response has an etag and
+                // it matches the etag we have from a previous response,
+                // then we should update our stored copy's headers from
+                // the new response. same goes for last-modified. but
+                // if those headers match then there's nothing to update
+                // since we don't track any other headers yet. so, like
+                // 5xx responses, 304 should preserve the old response
+                // unchanged.
             }
-        });
-        result
+
+            // only count it as a successful fetch if we actually got
+            // a response from the server, because when we have network
+            // trouble or similar, it's possible that we miss a change.
+            last_fetched = request_time;
+        }
+
+        // https://datatracker.ietf.org/doc/html/rfc7234#section-4.2.2
+        let last_changed_age = Instant::now().duration_since(last_changed).as_secs();
+        let heuristic_freshness = last_changed_age / 10;
+
+        // now we have two hints about the minimum time we should wait
+        // before checking again. max-age is the origin server's hint
+        // that a cache should not revalidate before that many seconds
+        // have elapsed. and our heuristic freshness is an estimate
+        // of how long we think it might be before it's worth checking
+        // again. so wait for whichever hint is longer, but also clamp
+        // to a reasonable range that neither waits forever nor sends
+        // another request immediately.
+        let wait = heuristic_freshness.max(max_age).clamp(600, 7 * DAY);
+
+        // sleep, then loop
+        tokio::time::sleep(Duration::from_secs(wait)).await;
     }
 }
 
